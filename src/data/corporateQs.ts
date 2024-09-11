9a64bec97a9df576b39b0fb69d34f75b9b82a7f5
suhayb <iamsuhaybahmed@gmail.com>
2024-09-11 20:37:47 +0530

    feat: add questions

--- src/data/corporateQs.ts
+++ src/data/corporateQs.ts
@@ -1,17 +1,23 @@
-export const prQs = ["PR Q1", "PR Q2", "PR Q3", "PR Q4", "PR Q5"];
+export const prQs = [
+  "Why do you wish to join this club and why do you think this domain is important for the club?",
+  "How would you promote a new event the club is organising? What steps would you take, and how would you decide what needs to be planned?",
+  "How would you narrate our club's story to appeal to new members while keeping current members engaged?",
+  "The event's registration numbers are lower than anticipated, and you're only a few days away from the event date. What actions would you take to boost attendance and ensure the event's success?",
+  "After an event, how would you assess its success?",
+];
 
 export const sponsorshipQs = [
-  "Sponsorship Q1",
-  "Sponsorship Q2",
-  "Sponsorship Q3",
-  "Sponsorship Q4",
-  "Sponsorship Q5",
+  "Why do you wish to join this club and why do you think this domain is important for the club?",
+  "Tell me how you will manage if the budgets are cut short at the last moment of the event.",
+  "You're tasked with approaching a brand that's never partnered with a student club before. How would you pitch our club in a way that they simply can't refuse?",
+  "You're in the middle of a sponsorship negotiation, and the sponsor is hesitant. What creative approach would you use to turn their hesitation into enthusiasm?",
+  "During a sponsorship discussion, if a sponsor asks for extra benefits that weren't originally agreed upon, how would you handle this while protecting the club's interests? ",
 ];
 
 export const eventsQs = [
-  "Sponsorship Q1",
-  "Sponsorship Q2",
-  "Sponsorship Q3",
-  "Sponsorship Q4",
-  "Sponsorship Q5",
+  "Why do you wish to join this club and why do you think this domain is important for the club?",
+  "What will be your first decisions if you see there is an issue between students in the event that is disturbing the event flow.",
+  "What will you do when the capacity of students is exceeding and you don't have any space left. What will be your response? How will you convince them that you can't give them an OD but they are already there leaving their class.",
+  "What will be your response if the refreshments are finished but there are still students left. How will you manage it?",
+  "Suppose you need to organise a technical event, give me a rough flow plan , how will you plan it.",
 ];
--- src/data/techQs.ts
+++ src/data/techQs.ts
@@ -1,17 +1,17 @@
 export const webDevQs = [
-  "Web Dev Q1",
-  "Web Dev Q2",
-  "Web Dev Q3",
-  "Web Dev Q4",
-  "Web Dev Q5",
+  "Name web apps that fascinate you the most. What features or aspects of these websites do you find particularly intriguing?",
+  "Can you walk me through a project where you faced a significant challenge? How did you overcome it?",
+  "What are the main technologies or tools used in Web Development?",
+  "Explain the differences between HTML, CSS, and JavaScript. How do they interact to create a web page?",
+  "What is your understanding of Client and Server in a Web App. How do they interact and what is the main difference between the two",
 ];
 
 export const appDevQs = [
-  "App Dev Q1",
-  "App Dev Q2",
-  "App Dev Q3",
-  "App Dev Q4",
-  "App Dev Q5",
+  "In your opinion, what is App Development? How would you describe it in simple terms?",
+  "What are the main technologies or tools used in App Development?",
+  "Name three apps that fascinate you the most. What features or aspects of these apps do you find particularly intriguing?",
+  "If you were to develop an app, what problem would it solve, and how would it work?",
+  "Can you think of a unique feature that you would like to see in an app? Why do you think it's important?",
 ];
 
 export const mlQs = ["ML Q1", "ML Q2", "ML Q3", "ML Q4", "ML Q5"];
