c2bc8baa472126f871d57dd517702eb16cc97ec2
suhayb <iamsuhaybahmed@gmail.com>
2024-09-13 14:24:46 +0530

    fix: ML qs

--- src/data/techQs.ts
+++ src/data/techQs.ts
@@ -14,4 +14,10 @@ export const appDevQs = [
   "Can you think of a unique feature that you would like to see in an app? Why do you think it's important?",
 ];
 
-export const mlQs = ["ML Q1", "ML Q2", "ML Q3", "ML Q4", "ML Q5"];
+export const mlQs = [
+  "What are the key differences between supervised and unsupervised learning?",
+  "Explain a real-world application of Machine Learning that interests you and why.",
+  "Describe the concept of overfitting in Machine Learning. How can it be prevented?",
+  "What is the purpose of cross-validation in Machine Learning?",
+  "What are the advantages and disadvantages of using a deep learning model over a traditional ML algorithm?",
+];
