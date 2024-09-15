30f1a9be10cfbd4a61af893b03cc9ea430a38727
suhayb <iamsuhaybahmed@gmail.com>
2024-09-15 23:11:07 +0530

    feat: add community link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "@/assets/logog.svg";
 import logonew from "@/assets/logonew.svg";
-
+import whatsapp from "@/assets/wa.svg";
 import Image from "next/image";
 import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className="flex w-full justify-start items-center bg-black h-16 px-10 py-10">
+    <div className="flex w-full justify-between items-center bg-black h-16 px-10 py-10">
       <Link href="/">
         <div className="flex justify-center items-center gap-2">
           <Image
@@ -18,6 +19,9 @@ const Navbar = () => {
           />
           <h1 className="text-white font-bold text-xl">dBug Labs</h1>
         </div>
+      </Link>
+      <Link href="https://chat.whatsapp.com/FMAMZoWoHUV23fTTFeNYB4">
+        <Image src={whatsapp} alt="" height={0} width={0} className="h-8 w-8" />
       </Link>
     </div>
   );
